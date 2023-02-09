c9821ebc6d597dec1a4426a5d815231aefaee0d6
Jafar <jafar@idmelon.co>
2023-02-09 16:24:24 +0330

    First Commit

---
+++ DispatchQueue/DispatchQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DispatchQueue
+{
+    public class DispatchQueue
+    {
+        private Task queue;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public static DispatchQueue main = new DispatchQueue();
+
+        public DispatchQueue()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            queue = new Task(() => { }, cancellationTokenSource.Token, TaskCreationOptions.RunContinuationsAsynchronously);
+            queue.Start();
+        }
+
+        public async void Async(Action<Task> action)
+        {
+            await queue.ContinueWith(action);
+        }
+
+        public void Sync(Action<Task> action)
+        {
+            queue.ContinueWith(action);
+        }
+    }
+}
