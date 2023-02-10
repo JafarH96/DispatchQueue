8abf50dbd05e1ed50542a2a0eeaa2b7ec0740a84
Jafar <jafarheidary75@gmail.com>
2023-02-10 13:50:52 +0330

    Add .Net Core Library

---
+++ SerialDispatchQueue/SerialQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SerialDispatchQueue
+{
+    public class SerialQueue
+    {
+        private Task queue;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public static SerialQueue main = new SerialQueue();
+
+        public SerialQueue()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            queue = new Task(() => { }, cancellationTokenSource.Token, TaskCreationOptions.RunContinuationsAsynchronously);
+            queue.Start();
+        }
+
+        public async void Async(Action<Task> action)
+        {
+            await queue.ContinueWith(action);
+        }
+
+        public async void AsyncAfter(int milliseconds, Action<Task> action)
+        {
+            await Task.Delay(milliseconds);
+            await queue.ContinueWith(action);
+        }
+
+        public void Sync(Action<Task> action)
+        {
+            queue.ContinueWith(action);
+        }
+    }
+}
